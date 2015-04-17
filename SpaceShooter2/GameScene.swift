700a1ba22a5d2bf45967a8ed0bfa9e37b4ef3343
Hrududu8 <rkorde@gmail.com>
2015-04-16 20:48:22 -0400

    Figured why the score labels and buttons keep disappearing.  Still have to fix the problem.

--- SpaceShooter2/GameScene.swift
+++ SpaceShooter2/GameScene.swift
@@ -8,13 +8,28 @@
 
 import SpriteKit
 
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     let screenWidth = UIScreen.mainScreen().bounds.size.width
     let screenHeight = UIScreen.mainScreen().bounds.size.height
     
+    let scoreLabelNode = SKLabelNode()
     var shipHealth = 100
     
-    func randomCGFloat(min: CGFloat, max: CGFloat)->CGFloat{
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        scoreLabelNode.position = CGPoint(x: 40, y: 40)
+        scoreLabelNode.name = "shello"
+        scoreLabelNode.fontColor = SKColor.whiteColor()
+        scoreLabelNode.fontSize = 30
+        scoreLabelNode.zPosition = 30 
+        scoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Right
+        scoreLabelNode.text = "hello world"
+        addChild(scoreLabelNode)
+        
+    }
+    
+        func randomCGFloat(min: CGFloat, max: CGFloat)->CGFloat{
         return CGFloat(arc4random()) % max - min
     }
     
@@ -26,6 +41,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let safeNode = aNode as! SKSpriteNode
             safeNode.physicsBody?.velocity.dx = self.randomCGFloat(0.0, max: 200) - 100
             safeNode.physicsBody?.velocity.dy = self.randomCGFloat(0.0, max: 200) - 100
+            safeNode.physicsBody?.angularVelocity = self.randomCGFloat(0, max: 4) - 2
             safeNode.physicsBody?.restitution = 0.2
             safeNode.physicsBody?.categoryBitMask = 1
         })
@@ -36,28 +52,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             safeNode.physicsBody?.restitution = 0.2
             safeNode.physicsBody?.contactTestBitMask = 3
         })
-        let node = childNodeWithName("scoreLabelNode")
-        if let safeNode = node as? SKLabelNode {
-            println("safeNode.alpha = \(safeNode.alpha)")
-            
-        }
+        
     }
+    
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
      
         /* Called when a touch begins
         the code should moved to init or didMoveToView becuase now it runs everytime there's a touch
         */
-        
+        let rotate = SKAction.rotateByAngle(0.34, duration: 0.5)
+        let rotateShip = SKAction.runAction(rotate, onChildWithName: "shipNode")
+        let buttonParentNode = childNodeWithName("buttonParentNode")
+        let leftArrowButton = buttonParentNode!.childNodeWithName("leftArrowButton")
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
-            let nodes = nodesAtPoint(location)
-            let rotate = SKAction.rotateByAngle(0.34, duration: 0.5)
-            let rotateShip = SKAction.runAction(rotate, onChildWithName: "shipNode")
-            //TODO change this from an enumearate ChildNodesWithName: using Block to something better
-            enumerateChildNodesWithName("*Button", usingBlock: { (aNode : SKNode!, anUnsafePointer : UnsafeMutablePointer<ObjCBool>) -> Void in
+            if (leftArrowButton!.containsPoint(location)){ //TODO:  fix this so it recognizes when the touches are in the box
                 self.runAction(rotateShip)
-        })
+            }
         }
     }
     func checkIfHeadingOffscreen(aNode: SKNode!, anUnsafePoint: UnsafeMutablePointer<ObjCBool>) -> Void {
@@ -67,8 +79,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if (aNode.position.x > (self.screenWidth - 10)) {aNode.position.x = 10}
     }
     
+    func spriteDebug(aNode: SKNode!, anUnsafePointer: UnsafeMutablePointer<ObjCBool>)->Void {
+        println("--\(aNode.name) at zPosition \(aNode.zPosition)")
+        }
+        
+        
+    
     override func update(currentTime: CFTimeInterval) {
-        enumerateChildNodesWithName("//*", usingBlock: checkIfHeadingOffscreen)
+        enumerateChildNodesWithName("//*", usingBlock: checkIfHeadingOffscreen)  // why is this line of code moving my labels?????
+        enumerateChildNodesWithName("//*", usingBlock: spriteDebug)
         enumerateChildNodesWithName("asteriodNode", usingBlock: { (aNode : SKNode!, anUnsafePointer : UnsafeMutablePointer<ObjCBool>) -> Void in
             let safeNode = aNode as! SKSpriteNode
             if (safeNode.physicsBody?.linearDamping != 0){
